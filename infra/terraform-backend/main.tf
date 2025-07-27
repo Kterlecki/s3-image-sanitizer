10013ea3dfcf78de304012943807d95bdf3c1205
Kacper Terlecki <kacper.terlecki@pfizer.com>
2025-07-27 20:00:23 +0100

    terraform back-end implementation

---
+++ infra/terraform-backend/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket = "imagesanitizer-s3-state"
+    key    = "dev/terraform-backend/terraform.tfstate"
+    region = "us-east-1"
+
+    use_lockfile   = true
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
