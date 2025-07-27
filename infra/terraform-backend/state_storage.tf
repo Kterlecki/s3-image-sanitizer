51c567839d9c52b5ac8d79e5ac3bc1e2d0275776
Kacper Terlecki <kacper.terlecki@pfizer.com>
2025-07-27 20:06:58 +0100

    update path to module tf storage

--- infra/terraform-backend/state_storage.tf
+++ infra/terraform-backend/state_storage.tf
@@ -1,4 +1,4 @@
 module "imagesanitizer_tf_state_storage" {
-  source                                 = "../../../modules/tf-state-storage"
+  source                                 = "../modules/tf-state-storage"
   project                                = var.project
 }
