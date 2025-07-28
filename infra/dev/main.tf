0f24aae9fb2a7ff54296656e29bc1fb71cb24f3d
Kacper Terlecki <kacper.terlecki@pfizer.com>
2025-07-28 12:08:28 +0100

    add dynamic referencing for networking resources and data query for ecr

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -12,3 +12,43 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+data "aws_vpc" "imagesanitizer" {
+  # VPC shouldnt be created in this project, as a result we are using a data source to query the existing VPC.
+  # So this query selects VPC.
+  default = false
+}
+
+data "aws_subnets" "all" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.imagesanitizer.id]
+  }
+}
+
+data "aws_subnet" "selected_subnet" {
+  for_each = toset(local.subnet_ids)
+  id       = each.value
+}
+
+locals {
+  # Assuming multiple subnets are available, we select the first and the last one for redundancy.
+  # This is a simplistic approach; in production, you might want to select subnets based on specific criteria.
+  # Ensure that the selected subnets are in different availability zones for high availability.
+  subnet_ids = [
+    data.aws_subnets.all.ids[1],
+    data.aws_subnets.all.ids[length(data.aws_subnets.all.ids) - 4]
+  ]
+}
+
+module "label_ecr" {
+  source      = "../../modules/label"
+  type        = "ecrrepository"
+  project     = var.project
+  environment = "crossenv"
+  attribute   = "artifactstorage"
+}
+
+data "aws_ecr_repository" "imagesanitizer" {
+  name = module.label_ecr.repository_name
+}
